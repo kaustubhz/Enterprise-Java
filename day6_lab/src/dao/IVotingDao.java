4da150454d2ff84f094afb18131be32f49e3105f
Kaustubh Zagade <38424988+kaustubhz@users.noreply.github.com>
2019-11-16 21:49:15 +0530

    Add files via upload

---
+++ day6_lab/src/dao/IVotingDao.java
@@ -0,0 +1,14 @@
+package dao;
+
+import java.util.List;
+
+import pojos.Candidate;
+import pojos.Voter;
+
+public interface IVotingDao {
+	
+	Voter authenticateVoter(String em,String pass) throws Exception;
+	List<Candidate> getCandidateList() throws Exception;
+	String incVotesUpdateStatus(int candidateId,int voterId) throws Exception;
+	void cleanUp() throws Exception;
+}
---
+++ day6_lab/src/pojos/Candidate.java
@@ -0,0 +1,46 @@
+package pojos;
+
+public class Candidate {
+
+	private int id; 
+	private String name,politicalParty;
+	private int votes;
+	
+
+	public Candidate(int id, String name, String politicalParty, int votes) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.politicalParty = politicalParty;
+		this.votes = votes;
+	}
+	
+
+	@Override
+	public String toString() {
+		return "Candidate [name=" + name + ", politicalParty=" + politicalParty + ", votes=" + votes + "]";
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+
+	public String getName() {
+		return name;
+	}
+
+
+	public String getPoliticalParty() {
+		return politicalParty;
+	}
+
+
+	public int getVotes() {
+		return votes;
+	}
+
+	
+	
+}
